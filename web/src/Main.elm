b6b1c9c2957443eaa2b900b91ba9f7e16b33fe29
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-03-16 18:13:03 +0100

    Basic HTTP request from Elm to API.
    See https://www.reddit.com/r/elm/comments/48ixdc/whats_the_absolute_bareminimum_for_a_http_request/

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -1,6 +1,15 @@
 module Main where
 
 import Html exposing (text)
+import Http
+import Task exposing (Task, andThen)
+
+
+results = Signal.mailbox ""
+
+port tasks : Task Http.Error ()
+port tasks =
+    Http.getString "/api/" `andThen` Signal.send results.address
 
 main =
-  text "Hello, World!"
+    Signal.map text results.signal
