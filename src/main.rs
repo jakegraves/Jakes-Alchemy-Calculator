9550518bbcc64824bf128eca10e64d977f43d21c
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-03-17 18:44:33 +0100

    Read JSON data from file.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate mount;
 extern crate logger;
 extern crate staticfile;
 
+use std::fs::File;
+use std::io::Read;
+
 use iron::prelude::*;
 use iron::mime::Mime;
 use iron::status;
@@ -10,27 +13,13 @@ use mount::Mount;
 use logger::Logger;
 use staticfile::Static;
 
-static JSON_RESPONSE: &'static str = "{
-    \"ingredients\": [
-        {
-            \"name\": \"Creep Cluster\",
-            \"value\": 1,
-            \"weight\": 0.2,
-            \"image\": \"http://www.uesp.net/w/images/1/15/SR-icon-ingredient-Creep_Cluster.png\",
-            \"effects\": [
-                \"Restore Magicka\",
-                \"Damage Stamina Regen\",
-                \"Fortify Carry Weight\",
-                \"Weakness to Magic\"
-            ]
-        }
-    ]
-}";
-
 fn main() {
     fn ingredients(_: &mut Request) -> IronResult<Response> {
+        let mut json_response = String::new();
+        let mut f = File::open("data/ingredients.json").unwrap();
+        f.read_to_string(&mut json_response).unwrap();
         let content_type = "application/json".parse::<Mime>().unwrap();
-        Ok(Response::with((content_type, status::Ok, JSON_RESPONSE)))
+        Ok(Response::with((content_type, status::Ok, json_response)))
     }
     let mut mount = Mount::new();
     mount.mount("/", Static::new("web/"));
