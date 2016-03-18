ad48640442fcc8e4fabddbc8a60fd94ee6213d9c
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-03-18 22:04:05 +0100

    Add Bootstrap 4 stylesheet.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
-        app: './web/src/app.js'
+        app: './web/src/app.js',
+        style: './web/style.css'
     },
     output: {
         path: './web/build',
@@ -13,7 +15,13 @@ module.exports = {
             test: /\.elm$/,
             exclude: [/elm-stuff/, /node_modules/],
             loader: 'elm-webpack'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
         }],
         noParse: [/\.elm$/]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+    ]
 };
