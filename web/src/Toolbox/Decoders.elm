fc9436aa8bdbb4353a1ed6c1f6f9976c9dcce07c
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-04-10 17:06:07 +0200

    Use |: from elm-json-extra to avoid fixed number of fields in decoders.

--- web/src/Toolbox/Decoders.elm
+++ web/src/Toolbox/Decoders.elm
@@ -1,20 +1,21 @@
 module Toolbox.Decoders (..) where
 
-import Json.Decode exposing ((:=), Decoder, string, int, float, list, object1, object5)
+import Json.Decode exposing ((:=), Decoder, string, int, float, list, succeed)
+import Json.Decode.Extra exposing ((|:))
 import Toolbox.Types exposing (Ingredient, Response)
 
 
 ingredient : Decoder Ingredient
 ingredient =
-  object5
-    Ingredient
-    ("name" := string)
-    ("value" := int)
-    ("weight" := float)
-    ("image" := string)
-    ("effects" := list string)
+  succeed Ingredient
+    |: ("name" := string)
+    |: ("value" := int)
+    |: ("weight" := float)
+    |: ("image" := string)
+    |: ("effects" := list string)
 
 
 response : Decoder Response
 response =
-  object1 Response ("ingredients" := list ingredient)
+  succeed Response
+    |: ("ingredients" := list ingredient)
